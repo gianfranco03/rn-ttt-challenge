65b67d63537456639935db830b6fea7d911f1ca4
gianfranco03 <gianfrancohj03@gmail.com>
2026-02-01 17:37:58 -0600

    feat: uniwind styles setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,14 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from '@react-navigation/native';
 import React from 'react';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import '../../global.css';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
